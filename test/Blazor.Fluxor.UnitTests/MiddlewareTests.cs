3f3caaeffe9ec3bcb4ffa66f5a0b1b82350e2ed5
Peter Morris <mrpmorris@gmail.com>
2018-06-16 21:11:28 +0100

    Middleware.BeginInternalMiddlewareChange tests

---
+++ test/Blazor.Fluxor.UnitTests/MiddlewareTests.cs
@@ -0,0 +1,43 @@
+﻿using Blazor.Fluxor.UnitTests.SupportFiles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Blazor.Fluxor.UnitTests
+{
+	public class MiddlewareTests
+	{
+		[Fact]
+		public void IsInsideMiddlewareChange_ShouldBeTrue_UntilBeginInternalMiddlewareChangeIsDisposed()
+		{
+			var subject = new MiddlewareWithExposedMembers();
+			var disposables = new Queue<IDisposable>();
+
+			Assert.Equal(0, subject._BeginMiddlewareChangeCount);
+			Assert.False(subject._IsInsideMiddlewareChange);
+
+			int expectedChangeCount = 0;
+			for(int i = 1; i <= 10; i++)
+			{
+				expectedChangeCount = i;
+				IDisposable disposable = subject._BeginInternalMiddlewareChange();
+				disposables.Enqueue(disposable);
+
+				Assert.Equal(expectedChangeCount, subject._BeginMiddlewareChangeCount);
+				Assert.True(subject._IsInsideMiddlewareChange);
+			}
+			do
+			{
+				Assert.Equal(expectedChangeCount, subject._BeginMiddlewareChangeCount);
+				Assert.True(subject._IsInsideMiddlewareChange);
+
+				disposables.Dequeue().Dispose();
+				expectedChangeCount--;
+			} while (disposables.Count > 0);
+
+			Assert.Equal(0, subject._BeginMiddlewareChangeCount);
+			Assert.False(subject._IsInsideMiddlewareChange);
+		}
+	}
+}
---
+++ test/Blazor.Fluxor.UnitTests/SupportFiles/MiddlewareWithExposedMembers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blazor.Fluxor.UnitTests.SupportFiles
+{
+	public class MiddlewareWithExposedMembers : Middleware
+	{
+		public int _BeginMiddlewareChangeCount => BeginMiddlewareChangeCount;
+		public bool _IsInsideMiddlewareChange => IsInsideMiddlewareChange;
+		public IDisposable _BeginInternalMiddlewareChange() =>
+			((IMiddleware)this).BeginInternalMiddlewareChange();
+	}
+}
