811792b1a2c74d78b6cd23f5e3b482592bcb9dfe
Peter Morris <mrpmorris@gmail.com>
2018-06-16 21:55:58 +0100

    DisposableAction tests

---
+++ test/Blazor.Fluxor.UnitTests/DisposableCallbackTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace Blazor.Fluxor.UnitTests
+{
+	public class DisposableCallbackTests
+	{
+		[Fact]
+		public void Dispose_CallsActionPassedInConstructor()
+		{
+			bool wasCalled = false;
+			Action action = () => wasCalled = true;
+			var subject = new DisposableCallback(action);
+
+			Assert.False(wasCalled);
+			subject.Dispose();
+			Assert.True(wasCalled);
+		}
+	}
+}
